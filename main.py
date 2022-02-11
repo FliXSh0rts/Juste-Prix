0e65bd98b2ce73b5e72d4d88c5c180aa1e9cedb3
FliXSh0rts <99490115+FliXSh0rts@users.noreply.github.com>
2022-02-11 15:37:28 +0100

    first commit

---
+++ main.py
@@ -0,0 +1,9 @@
+from game import JustePrix
+from tkinter import Tk, Label, Frame
+from frame_finish import FrameFinish
+
+if __name__ == '__main__':
+
+    main_window = JustePrix()
+
+    main_window.mainloop()
